55383f708aa41d2288a83658b9c7114a017c17a7
Patrick Doane <patrickdoane@gmail.com>
2024-09-18 22:34:38 -0500

    Add react client

---
+++ grail-client/src/App.js
@@ -0,0 +1,17 @@
+import logo from './d2rlogo2.png';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Holy Grail Tracker for Diablo II: Resurrected
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
