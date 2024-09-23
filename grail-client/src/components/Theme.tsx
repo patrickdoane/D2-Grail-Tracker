e7e9e7656ecdd1d5d42e611fc8563ce03ddbb3f0
Patrick Doane <patrickdoane@gmail.com>
2024-09-23 16:35:25 -0500

    demo DataGrid component

--- grail-client/src/components/Theme.tsx
+++ grail-client/src/components/Theme.tsx
@@ -1,11 +1,21 @@
 import { grey, brown } from "@mui/material/colors";
 
+import type {} from '@mui/x-data-grid/themeAugmentation';
+
 const theme = {
   palette: {
     primary: grey,
     secondary: brown,
   },
-
+   components: {
+    // Use `MuiDataGrid` on DataGrid, DataGridPro and DataGridPremium
+    MuiDataGrid: {
+      styleOverrides: {
+        root: {
+        },
+      },
+    },
+  },
 }
 
 export default theme;
